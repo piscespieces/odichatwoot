300522dc1ca8f8a35b6a99d8e5d22659d108f4a0
Andres <aeum3893@gmail.com>
2025-12-22 15:39:47 -0600

    refactor: disable business email validation for signup form email field

--- app/javascript/v3/views/auth/signup/components/Signup/Form.vue
+++ app/javascript/v3/views/auth/signup/components/Signup/Form.vue
@@ -12,7 +12,7 @@ import Icon from 'dashboard/components-next/icon/Icon.vue';
 import { isValidPassword } from 'shared/helpers/Validators';
 import GoogleOAuthButton from '../../../../../components/GoogleOauth/Button.vue';
 import { register } from '../../../../../api/auth';
-import * as CompanyEmailValidator from 'company-email-validator';
+// import * as CompanyEmailValidator from 'company-email-validator';
 
 const MIN_PASSWORD_LENGTH = 6;
 const SPECIAL_CHAR_REGEX = /[!@#$%^&*()_+\-=[\]{}|'"/\\.,`<>:;?~]/;
@@ -58,9 +58,9 @@ export default {
         email: {
           required,
           email,
-          businessEmailValidator(value) {
-            return CompanyEmailValidator.isCompanyEmail(value);
-          },
+          // businessEmailValidator(value) {
+          //   return CompanyEmailValidator.isCompanyEmail(value);
+          // },
         },
         password: {
           required,
